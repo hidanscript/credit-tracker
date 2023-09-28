d1c89ffd9d50bf47f6a3bc23a6aa0f17c7a7ffc9
David Ortiz <dmarcano.ml@gmail.com>
2023-09-28 04:56:16 -0300

    Fixed logs

--- routes/api/log.ts
+++ routes/api/log.ts
@@ -1,17 +1,29 @@
 import { Handlers } from "$fresh/server.ts";
 
 interface Log {
-    id: number,
-    date: Date,
-    amount: number | string,
-    type: string,
+    id: string | number | null,
+    date: Date | string | null,
+    amount: number | string | null,
+    type: string | null,
 }
 
 export const handler: Handlers<Log | null> = {
     async POST(req, _ctx) {
       const kv = await Deno.openKv();
-      const log = (await req.json()) as Log;
+      const id = getPropertyFromUrl(req.url, "id");
+      const date = getPropertyFromUrl(req.url, "date");
+      const type = getPropertyFromUrl(req.url, "type");
+      const amount = getPropertyFromUrl(req.url, "amount");
+
+      const log: Log = {
+        id,
+        date,
+        type,
+        amount
+      }
+
       const logKey = ["log", log.id];
+      console.log('log', log)
       const ok = await kv.atomic().set(logKey, log).commit();
       if (!ok) throw new Error("Something went wrong.");
       return new Response(JSON.stringify(log));
@@ -22,4 +34,9 @@ export const handler: Handlers<Log | null> = {
       const entries = kv.list({ prefix: ["log"] });
       return new Response(JSON.stringify(entries));
     },
+  };
+
+  const getPropertyFromUrl = (url: string, property: string) => {
+    const urlObject = new URL(url);
+    return urlObject.searchParams.get(property);
   };
